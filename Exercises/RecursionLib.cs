9d5ed526d79aad89c78e417adc4d38d74db348d0
Nathan <102090809@student.swin.edu.au>
2019-11-30 11:57:32 +1100

    RecursionLib Commit Test

--- Exercises/RecursionLib.cs
+++ Exercises/RecursionLib.cs
@@ -1,49 +1,54 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Exercises
 {
-    public class RecursionLib {
-
-        public int FactorialRec(int num, int ans = 1) {
-
-            if (num == 0) {
+    public class RecursionLib
+    {
+        public int FactorialRec(int num, int ans = 1)
+        {
+            if (num == 0)
+            {
                 return ans;
             }
 
-            return FactorialRec(num-1, ans*num);
-            // return num*FactorialRec(num-1)
+            return FactorialRec(num - 1, ans * num);
         }
 
-        /// Creates a list of numbers from 1 to num.  Initialise numsList with empty List.
-        /// </summary>
-        /// <param name="num"></param>
-        /// <param name="numsList"></param>
-        /// <returns></returns>
-        public List<int> CountRec(int num, List<int> numsList) {
-            return new List<int>();
+        public List<int> CountRec(int num, List<int> numsList)
+        {
+            int lCount = numsList.Count;
+            if (lCount == num)
+            {
+                return numsList;
+            }
+
+            numsList.Add(lCount + 1);
+            return CountRec(num, numsList);
+        }
+        public long ExponentialRec(int num, int pow, long ans = 1)
+        {
+            if (pow == 0)
+            {
+                return ans;
+            }
+            ans = ans * num;
+
+            pow--;
+            return ExponentialRec(num, pow, ans);
         }
 
-        /// Returns the result of num to the power of pow.
-        /// </summary>
-        /// <param name="num"></param>
-        /// <param name="pow"></param>
-        /// <param name="ans"></param>
-        /// <returns></returns>
-        public long ExponentialRec(int num, int pow, long ans = 1) {
-            return 1;
+        public string WordReverseRec(string word, string revWord = "")
+        {
+            int counter = revWord.Length;
+            if (revWord.Length == word.Length)
+            {
+                return revWord;
+            }
+
+            revWord += word[word.Length - 1 - counter];
+            return WordReverseRec(word, revWord);
         }
-
-        /// Returns word (string) in reverse order.  Treats all characters the same.
-        /// </summary>
-        /// <param name="word"></param>
-        /// <param name="revWord"></param>
-        /// <returns></returns>    
-        public string WordReverseRec(string word, string revWord = "") {
-            return "";
-        }
-
-
-
     }
 }
